7d0ec069a08249362d35178eb3d88a7addb322a5
albim72 <marcin.albiniak72@gmail.com>
2022-12-21 20:02:41 +0100

    Create GreetingFunctional.tsx

---
+++ DZIEN_3/GREET_HOOKS/src/GreetingFunctional.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import {atob} from "buffer";
+
+interface GreetingProps{
+    enteredName:string,
+    message: string,
+    greetingDispatcher: React.Dispatch<{ type:string,payload:string }>
+}
+
+export default function Greeting(props:GreetingProps){
+    console.log("Renderuję komponent Greeting")
+    const onChangeName = (e:React.ChangeEvent<HTMLInputElement>) => {
+        props.greetingDispatcher({type:"enteredName",payload:e.target.value})
+        props.greetingDispatcher({type:"message",payload:e.target.value})
+    }
+    return (
+        <div>
+            <input value={props.enteredName} onChange={onChangeName}/>
+            <div>
+                {props.message}
+            </div>
+        </div>
+    )
+}
